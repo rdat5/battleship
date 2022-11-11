0c99037f4ccd247b89c2a45772f2512322437e97
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-11-10 17:40:57 -0800

    Add GameBoard class

---
+++ src/Gameboard.js
@@ -0,0 +1,47 @@
+const Gameboard = (boardSize = 10) => {
+    function createBoard(size) {
+        let newBoard = []
+        for (let i = 0; i < size; i++) {
+            let row = [];
+            for (let j = 0; j < size; j++) {
+                row.push(null);
+            }
+            newBoard.push(row);
+        }
+        return newBoard;
+    }
+    
+    return {
+        board: createBoard(boardSize),
+        missedShots: [],
+        placedShips: [],
+        placeShip(x, y, ship, isVertical) {
+            if (isVertical){
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[y + i][x] = ship;
+                }
+            }
+            else {
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[y][x + i] = ship;
+                }
+            }
+            this.placedShips.push(ship);
+        },
+        receiveAttack(x, y) {
+            if (this.board[y][x]) {
+                this.board[y][x].hit();
+            }
+            else {
+                this.missedShots.push([x, y]);
+            }
+        },
+        areShipsAllSunk() {
+            return this.placedShips.every((s) => {
+                return s.isSunk();
+            })
+        }
+    }
+};
+
+export default Gameboard;
