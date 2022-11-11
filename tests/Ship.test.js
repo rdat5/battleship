e03a978cf8ce9a4948fc6f16bb96e39b1dafe226
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-11-10 17:39:30 -0800

    Add test for ship to test if its sunk

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -5,3 +5,12 @@ test('Ship hit method increments timesHit', () => {
     testShip.hit();
     expect(testShip.timesHit).toEqual(1);
 });
+
+test('isSunk method checks if timesHit is equal to ship length', () => {
+    const testShip = Ship(2);
+    expect(testShip.isSunk()).toBe(false);
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(false);
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+})
