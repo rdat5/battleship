08ee565b7a4d19b216368274a13fe899f9704ab2
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-11-11 20:20:45 -0800

    Add test for Gameboards receiveAttack function

--- tests/GameBoard.test.js
+++ tests/GameBoard.test.js
@@ -14,3 +14,11 @@ test('Gameboard can place ships at specific coordinates', () => {
         ]
     );
 });
+
+test('receiveAttack function sends hit function to ship at coordinates', () => {
+    const testBoard = Gameboard(4);
+    const tShp = Ship(2);
+    testBoard.placeShip(1, 1, tShp, true);
+    testBoard.receiveAttack(1, 2);
+    expect(tShp.timesHit).toBe(1);
+});
