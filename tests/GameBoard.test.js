d4d72257f435bb807cbcbb2f6729e4845d0557ff
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-11-11 20:21:22 -0800

    Add test for tracking missed shots in Gameboard

--- tests/GameBoard.test.js
+++ tests/GameBoard.test.js
@@ -22,3 +22,15 @@ test('receiveAttack function sends hit function to ship at coordinates', () => {
     testBoard.receiveAttack(1, 2);
     expect(tShp.timesHit).toBe(1);
 });
+
+test('Gameboard keeps track of missed shots', () => {
+    const testBoard = Gameboard(4);
+    const tShp = Ship(2);
+    testBoard.placeShip(1, 1, tShp, true);
+    testBoard.receiveAttack(0, 3);
+    testBoard.receiveAttack(2, 2);
+    expect(testBoard.missedShots).toEqual([
+        [0, 3],
+        [2, 2]
+    ]);
+});
