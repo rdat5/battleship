96366ef3954a3245952b0504a93f8d9bf086b8e3
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-11-16 22:30:16 -0800

    Grid can draw ships on board

--- src/Components.js
+++ src/Components.js
@@ -8,6 +8,12 @@ function drawGrid(board) {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             const cell = document.createElement('button');
+            let cellData = board[i][j];
+            if (cellData === null) {
+            }
+            else {
+                cell.style.backgroundColor = 'grey';
+            }
             gridContainer.appendChild(cell);
         }
     }
