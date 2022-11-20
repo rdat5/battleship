985ef8ed039b64b5abd195586403abc1eae5f014
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-11-19 22:28:51 -0800

    Add test for isSunk function

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -8,3 +8,14 @@ test(`Ship's hit function increments hits`, () => {
 
     expect(testShip.timesHit).toBe(2);
 });
+
+test(`Ship's isSunk function can tell if it's sunk`, () => {
+    const testShip = Ship(4);
+
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+
+    expect(testShip.isSunk()).toBe(true);
+});
